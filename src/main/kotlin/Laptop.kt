a96dceb0a247e1e581d90ecd716550f752854fbf
Matt Thompson <matt34thompson@gmail.com>
2020-03-07 13:32:19 +0000

    Decorate base laptop with processor upgrade

--- src/main/kotlin/Laptop.kt
+++ src/main/kotlin/Laptop.kt
@@ -1,3 +1,27 @@
-class Laptop {
-    val description = ""
+fun main() {
+//    val laptop = Laptop()
+//
+//    println(laptop.processor.price)
+}
+
+const val BASE_PRICE = 1000.0
+
+interface Laptop {
+    val description: String
+    val price: Double
+}
+
+class BaseLaptop : Laptop {
+    override val description: String
+        get() = "Base laptop"
+
+    override val price: Double
+        get() = BASE_PRICE
+}
+
+class ProcessorUpgrade(private val baseLaptop: BaseLaptop) : Laptop by baseLaptop {
+    private val optionPrice = 149.99
+
+    override val price = baseLaptop.price + optionPrice
+    override val description = "${baseLaptop.description} @ $BASE_PRICE, with 18 core processor @ $optionPrice. Total: $price"
 }
--- src/test/kotlin/LaptopKtTest.kt
+++ src/test/kotlin/LaptopKtTest.kt
@@ -1,13 +1,25 @@
 import assertk.assertThat
 import assertk.assertions.isEmpty
+import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 
 class LaptopKtTest {
 
     @Test
-    fun `should create laptop with empty description when no options added`() {
-        val laptop = Laptop()
+    fun `should create laptop with empty description and base total price when no options added`() {
+        val laptop = BaseLaptop()
 
-        assertThat(laptop.description).isEmpty()
+        assertThat(laptop.description).isEqualTo("Base laptop")
+        assertThat(laptop.price).isEqualTo(BASE_PRICE)
+    }
+
+    @Test
+    fun `should create laptop with description and price that includes a processor upgrade`() {
+        val laptop = ProcessorUpgrade(BaseLaptop())
+
+        assertThat(laptop.description).isEqualTo(
+            "Base laptop @ 1000.0, with 18 core processor @ 149.99. Total: 1149.99"
+        )
+        assertThat(laptop.price).isEqualTo(1149.99)
     }
 }
