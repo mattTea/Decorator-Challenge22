ab90df854204f54a661158f817a122c340b4a65b
Matt Thompson <matt34thompson@gmail.com>
2020-03-07 16:36:30 +0000

    Make BaseLaptop default arg for decorators

--- src/main/kotlin/Laptop.kt
+++ src/main/kotlin/Laptop.kt
@@ -1,8 +1,13 @@
 fun main() {
-    val laptop = CaseUpgrade(BatteryUpgrade(GraphicsUpgrade(DriveUpgrade(MemoryUpgrade(ProcessorUpgrade(BaseLaptop()))))))
+    val laptopWithItAll = CaseUpgrade(BatteryUpgrade(GraphicsUpgrade(DriveUpgrade(MemoryUpgrade(ProcessorUpgrade())))))
 
-    println(laptop.description)
-    println("Total price: ${laptop.price}")
+    println(laptopWithItAll.description)
+    println("Total price: ${laptopWithItAll.price}")
+    println("-------")
+
+    val modestLaptop = MemoryUpgrade(CaseUpgrade())
+    println(modestLaptop.description)
+    println("Total price: ${modestLaptop.price}")
 }
 
 interface Laptop {
@@ -15,42 +20,42 @@ class BaseLaptop : Laptop {
     override val description = "Base laptop @ $price"
 }
 
-class ProcessorUpgrade(private val laptop: Laptop) : Laptop by laptop {
+class ProcessorUpgrade(private val laptop: Laptop = BaseLaptop()) : Laptop by laptop {
     private val optionPrice = 149.99
 
     override val price = laptop.price + optionPrice
     override val description = "${laptop.description}, 18 core processor @ $optionPrice"
 }
 
-class MemoryUpgrade(private val laptop: Laptop) : Laptop by laptop {
+class MemoryUpgrade(private val laptop: Laptop = BaseLaptop()) : Laptop by laptop {
     private val optionPrice = 49.99
 
     override val price = laptop.price + optionPrice
     override val description = "${laptop.description}, 1TB RAM @ $optionPrice"
 }
 
-class DriveUpgrade(private val laptop: Laptop) : Laptop by laptop {
+class DriveUpgrade(private val laptop: Laptop = BaseLaptop()) : Laptop by laptop {
     private val optionPrice = 109.99
 
     override val price = laptop.price + optionPrice
     override val description = "${laptop.description}, SSD @ $optionPrice"
 }
 
-class GraphicsUpgrade(private val laptop: Laptop) : Laptop by laptop {
+class GraphicsUpgrade(private val laptop: Laptop = BaseLaptop()) : Laptop by laptop {
     private val optionPrice = 29.99
 
     override val price = laptop.price + optionPrice
     override val description = "${laptop.description}, Lightning Graphics Card @ $optionPrice"
 }
 
-class BatteryUpgrade(private val laptop: Laptop) : Laptop by laptop {
+class BatteryUpgrade(private val laptop: Laptop = BaseLaptop()) : Laptop by laptop {
     private val optionPrice = 39.99
 
     override val price = laptop.price + optionPrice
     override val description = "${laptop.description}, Massive Battery @ $optionPrice"
 }
 
-class CaseUpgrade(private val laptop: Laptop) : Laptop by laptop {
+class CaseUpgrade(private val laptop: Laptop = BaseLaptop()) : Laptop by laptop {
     private val optionPrice = 59.99
 
     override val price = laptop.price + optionPrice
